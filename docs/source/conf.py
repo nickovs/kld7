02776cbaa05b6b75a01bb50d4fb343c35c4580d2
Nicko van Someren <nicko@nicko.org>
2020-08-24 18:07:33 -0600

    Fixed requirements for ReadTheDocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,7 +39,6 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.githubpages',
     'sphinx.ext.napoleon',
-    'sphinx_autodoc_typehints'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
